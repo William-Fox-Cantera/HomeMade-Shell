ecd1d18faad195b0b18b545b58693490df0f3726
BenSegal855 <bensegal@udel.edu>
2020-03-05 16:20:09 -0500

    Making it prety

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main( int argc, char **argv, char **envp ) {
 void sig_handler(int signal) {
   /* define your signal handler */
   //if(signal == SIGINT) 
+  printf("\nInterrupt\n");
   printShell();
-  printf(" Interrupt\n");
 }
 

0fdce43cc0dcff9776bf195aefcca8b828118355
BenSegal855 <bensegal@udel.edu>
2020-03-05 16:11:03 -0500

    Ctrl Z

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 
 int main( int argc, char **argv, char **envp ) {
   /* put signal set up stuff here */
-  if (signal(SIGINT, sig_handler) == SIG_ERR)
+  if (signal(SIGINT, sigint_handler) == SIG_ERR)
       printf(" Problem catching SIGINT\n");
   signal(SIGTSTP, sig_handler);
   signal(SIGTERM, sig_handler);
   return sh(argc, argv, envp);
 }
 
-void sig_handler(int signal) {
+void sigint_handler(int signal) {
   /* define your signal handler */
-  if(signal == SIGINT) 
+  //if(signal == SIGINT) 
       printf(" Interrupt\n");
+  
 }
 

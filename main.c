02191fc422a721eed5221039e97d89e10028618b
BenSegal855 <bensegal@udel.edu>
2020-03-05 16:12:12 -0500

    More of the ctrl Z

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 
 int main( int argc, char **argv, char **envp ) {
   /* put signal set up stuff here */
-  if (signal(SIGINT, sigint_handler) == SIG_ERR)
+  if (signal(SIGINT, sig_handler) == SIG_ERR)
       printf(" Problem catching SIGINT\n");
   signal(SIGTSTP, sig_handler);
   signal(SIGTERM, sig_handler);
   return sh(argc, argv, envp);
 }
 
-void sigint_handler(int signal) {
+void sig_handler(int signal) {
   /* define your signal handler */
   //if(signal == SIGINT) 
       printf(" Interrupt\n");
